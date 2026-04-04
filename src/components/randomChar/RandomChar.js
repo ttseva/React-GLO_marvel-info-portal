08606c9cc37213cb1fc44ecdfc8f39604d5a7fef
Anastasia S. <annihilcax@gmail.com>
2026-04-05 01:01:31 +0300

    feat: handle images

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -4,6 +4,7 @@ import MarvelService from "../../services/MarvelService";
 import Spinner from "../spinner/Spinner";
 
 import './randomChar.scss';
+import mjolnir from '../../resources/img/mjolnir.png'
 
 class RandomChar extends Component {
   state = {
@@ -11,11 +12,16 @@ class RandomChar extends Component {
     loading: true,
     error: false,
   }
+
   marvelService = new MarvelService();
 
-  constructor(props) {
-    super(props);
+  componentDidMount() {
     this.updateChar();
+    // this.timerId = setInterval(this.updateChar, 3000)
+  }
+
+  componentWillUnmount() {
+    // clearInterval(this.timerId);
   }
 
   onError = () => {
@@ -23,7 +29,12 @@ class RandomChar extends Component {
   }
 
   onCharLoader = (char) => {
-    this.setState({char, loading: false});
+    let img = new Image();
+
+    img.onload = () => {
+      this.setState({char, loading: false});
+    };
+    img.src = char.thumbnail;
   }
 
   updateChar = () => {
@@ -34,12 +45,11 @@ class RandomChar extends Component {
       .catch(this.onError);
   }
 
-
   render() {
     const {char, loading, error} = this.state;
 
     const errorMsg = error ? <ErrorMessage/> : null;
-    const spinner = loading ? <Spinner/> : null;
+    const spinner = (loading) ? <Spinner/> : null;
     const content = !(loading || error) ? <View char={char}/> : null;
 
     return (
@@ -55,10 +65,13 @@ class RandomChar extends Component {
           <p className="randomchar__title">
             Or choose another one
           </p>
-          <button className="button button__main">
+          <button className="button button__main" onClick={() => {
+            this.setState({loading: true});
+            this.updateChar()
+          }}>
             <div className="inner">try it</div>
           </button>
-          <img src="" alt="mjolnir" className="randomchar__decoration"/>
+          <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
         </div>
       </div>
     )
@@ -68,6 +81,10 @@ class RandomChar extends Component {
 const View = ({char}) => {
   const {name, description, homepage, thumbnail, wiki} = char;
 
+  let imgStyle = thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
+    ? {'objectFit': 'cover'}
+    : {'objectFit': 'contain'};
+
   const editDescription = (description) => {
     if (!description) return "No description yet...";
 
@@ -76,11 +93,11 @@ const View = ({char}) => {
       : description
   }
 
-  let desc = editDescription(description);
+  const desc = editDescription(description);
 
   return (
     <div className="randomchar__block">
-      <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+      <img src={thumbnail} alt="Random character" style={imgStyle} className="randomchar__img"/>
       <div className="randomchar__info">
         <p className="randomchar__name">{name}</p>
         <p className="randomchar__desc">{desc}</p>
--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import App from "./components/app/App";
 
 import "./style/style.scss";
 
-
-
-
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App/>);
