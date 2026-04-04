28ab66f3e437a3287243fb49bf2dea26adcdf7d3
Anastasia S. <annihilcax@gmail.com>
2026-04-04 21:33:39 +0300

    finish lesson 21

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,42 +1,78 @@
-import './randomChar.scss';
-import thor from '../../resources/img/thor.jpeg';
-import mjolnir from '../../resources/img/mjolnir.png';
+import {Component} from "react";
 
-const RandomChar = () => {
+import './randomChar.scss';
+import MarvelService from "../../services/MarvelService";
+
+class RandomChar extends Component {
+  state = {
+    char: {},
+  }
+  marvelService = new MarvelService();
+
+  constructor(props) {
+    super(props);
+    this.updateChar();
+  }
+
+  onCharLoader = (char) => {
+    this.setState({char});
+  }
+
+  updateChar = () => {
+    const id = Math.floor(Math.random() * 19);
+    this.marvelService
+      .getCharacter(id)
+      .then(this.onCharLoader)
+  }
+
+  editDescription = (description) => {
+    if (!description) return "Описание пока не добавили...";
+    if (description.trim().length > 40) return description.slice(0, 40) + '...';
+  }
+
+  render() {
+    const {char: {name, description, homepage, thumbnail, wiki}} = this.state;
+
+    let desc = description;
+    desc = this.editDescription(desc);
+
     return (
-        <div className="randomchar">
-            <div className="randomchar__block">
-                <img src={thor} alt="Random character" className="randomchar__img"/>
-                <div className="randomchar__info">
-                    <p className="randomchar__name">Thor</p>
-                    <p className="randomchar__descr">
-                        As the Norse God of thunder and lightning, Thor wields one of the greatest weapons ever made, the enchanted hammer Mjolnir. While others have described Thor as an over-muscled, oafish imbecile, he's quite smart and compassionate...
-                    </p>
-                    <div className="randomchar__btns">
-                        <a href="#" className="button button__main">
-                            <div className="inner">homepage</div>
-                        </a>
-                        <a href="#" className="button button__secondary">
-                            <div className="inner">Wiki</div>
-                        </a>
-                    </div>
-                </div>
+      <div className="randomchar">
+        <div className="randomchar__block">
+          <img src={
+            thumbnail
+          } alt="Random character" className="randomchar__img"/>
+          <div className="randomchar__info">
+            <p className="randomchar__name">{name}</p>
+            <p className="randomchar__descr">
+              {desc}
+            </p>
+            <div className="randomchar__btns">
+              <a href={homepage} className="button button__main">
+                <div className="inner">homepage</div>
+              </a>
+              <a href={wiki} className="button button__secondary">
+                <div className="inner">Wiki</div>
+              </a>
             </div>
-            <div className="randomchar__static">
-                <p className="randomchar__title">
-                    Random character for today!<br/>
-                    Do you want to get to know him better?
-                </p>
-                <p className="randomchar__title">
-                    Or choose another one
-                </p>
-                <button className="button button__main">
-                    <div className="inner">try it</div>
-                </button>
-                <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
-            </div>
+          </div>
         </div>
+        <div className="randomchar__static">
+          <p className="randomchar__title">
+            Random character for today!<br/>
+            Do you want to get to know him better?
+          </p>
+          <p className="randomchar__title">
+            Or choose another one
+          </p>
+          <button className="button button__main">
+            <div className="inner">try it</div>
+          </button>
+          <img src="" alt="mjolnir" className="randomchar__decoration"/>
+        </div>
+      </div>
     )
+  }
 }
 
 export default RandomChar;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import {createRoot} from "react-dom/client";
 import App from "./components/app/App";
-import MarvelService from "./services/MarvelService";
+
 import "./style/style.scss";
 
-const marvelService = new MarvelService();
 
-marvelService.getAllCharacters().then((response) => {response.data.results.forEach((result) => {console.log(result.name)})});
+
 
 const container = document.getElementById("root");
 const root = createRoot(container);
