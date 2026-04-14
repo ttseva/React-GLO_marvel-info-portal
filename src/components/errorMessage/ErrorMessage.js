ec3d1d3056540e07e55324d01cccacbf846b30dd
Anastasia S. <annihilcax@gmail.com>
2026-04-14 23:15:50 +0300

    Updates

--- src/components/errorMessage/ErrorMessage.js
+++
@@ -1,9 +0,0 @@
-import img from './error.gif'
-
-const ErrorMessage = () => {
-  return (
-    <img style={{display: "block", width: "64px", height: "64px", objectFit: "contain", margin: "0 auto"}} src={img} alt="error"/>
-  )
-}
-
-export default ErrorMessage;
